1d1723dbec0355026970a13bb8eec85b001963d1
Fernanda Rodrigues da Silva <fernanda.rodrigues.cdc@gmail.com>
2018-11-18 20:20:41 -0800

    2.9 Respondendo requisicoes com base na url requisitada

--- projects/project_1/noticias.js
+++ projects/project_1/noticias.js
@@ -1,7 +1,17 @@
 var http = require('http');
 
 var serverCallback = function(req,res){
-    res.end("<html><body>Portal de Notícias</body></html>");
+    var categoria = req.url;
+
+    if(categoria === '/tecnologia'){
+      res.end("<html><body>Portal de Tecnologia</body></html>");
+    } else if (categoria === '/moda'){
+      res.end("<html><body>Portal de Moda</body></html>");
+    } else if (categoria === '/beleza'){
+      res.end("<html><body>Portal de Beleza</body></html>");
+    } else {
+      res.end("<html><body>Portal de Notícias</body></html>");
+    }
 };
 
 var server = http.createServer(serverCallback);

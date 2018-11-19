00ae85c9854d4702a444cc48c523298ba9f48c22
Fernanda Rodrigues da Silva <fernanda.rodrigues.cdc@gmail.com>
2018-11-18 21:43:26 -0800

    3.14 NPM - Instalando o EJS

--- projects/project_1/app.js
+++ projects/project_1/app.js
@@ -1,13 +1,16 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res){
-  res.send("<html><body>Portal de noticias</body></html>");
-})
+app.set('view engine', "ejs");
 
 app.get('/tecnologia', function(req, res){
-  res.send("<html><body>Portal de tecnologia</body></html>");
-})
+  res.render("secao/tecnologia");
+});
+
+app.get('/', function(req, res){
+  res.send("<html><body>Portal de noticias</body></html>");
+});
+
 
 app.listen(3000, function(){
   console.log("Servidor rodando com Express");

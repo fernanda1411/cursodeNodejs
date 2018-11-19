b43e109d2bbb1c9b8bce4e7a8b5055debc5670cf
Fernanda Rodrigues da Silva <fernanda.rodrigues.cdc@gmail.com>
2018-11-18 20:18:06 -0800

    2.8 Respondendo requisicoes HTTP com NodeJS

--- projects/project_1/noticias.js
+++ projects/project_1/noticias.js
@@ -1,1 +1,9 @@
-console.log('Criando um site de notícias com NodeJS');
+var http = require('http');
+
+var serverCallback = function(req,res){
+    res.end("<html><body>Portal de Notícias</body></html>");
+};
+
+var server = http.createServer(serverCallback);
+
+server.listen(3000);
